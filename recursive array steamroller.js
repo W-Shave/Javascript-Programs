dcea8ee43c2355c7fb50421bdb801168971c842b
W-Shave <81817169+W-Shave@users.noreply.github.com>
2021-05-12 21:47:49 -0500

    Update recursive array steamroller.js

--- recursive array steamroller.js
+++ recursive array steamroller.js
@@ -1,10 +1,21 @@
-//This function flattens out any nested arrays so that all the arrays are in a single 1-dimensional array.
+/*This function flattens out any nested arrays
+so that all the arrays are in a single 
+1-dimensional array.*/
 function steamrollArray(arr) {
-	/*In the first step, a new variable is made from an empty array with all the first-level elements spread out in it. This takes the array one level deep, but may still leave some nested arrays.*/
+	/*In the first step, a new variable 
+        is made from an empty array with 
+        all the first-level elements spread out in it. 
+        This takes the array one level deep, 
+        but may still leave some nested arrays.*/
 	let newArr = [].concat(...arr);
 	//The console.log allows you to see the recursive process in action, step by step.
 	console.log(newArr);
-	/*The conditional checks if any nested arrays remain. If not, the function is done and returns newArr. If yes, it recursively calls itself and will keep doing so until all of the nested arrays are gone.*/
+	/*The conditional checks if any 
+        nested arrays remain. If not, 
+        the function is done and returns newArr. 
+        If yes, it recursively calls itself 
+        and will keep doing so until all of the 
+        nested arrays are gone.*/
 	if (newArr.some(Array.isArray)) {
 		return steamrollArray(newArr);
 	} else {
